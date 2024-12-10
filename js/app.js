c682c1cc4497eea0cd11e1ac3d085df62e957914
Bruno <brunolpw@gmail.com>
2024-12-10 01:37:24 -0300

    Adição das reformulações

--- js/app.js
+++ js/app.js
@@ -3,20 +3,22 @@ function main() {
     const canvas = document.getElementById('canvas');
     const fitnessCanvas = document.getElementById('fitnessCanvas');
 
-    const lblPopulation = document.getElementById('lblPopulation');
+    const lblGeneration = document.getElementById('lblGeneration');
     const lblFitness = document.getElementById('lblFitness');
     const lblBestChromosome = document.getElementById('lblBestChromosome');
+    const lblIndividualFitness = document.getElementById('lblIndividualFitness');
 
     const buttonPopulation = document.getElementById('btnNextPopulation');
     
     const context = canvas.getContext('2d');
     const fitnessContext = fitnessCanvas.getContext('2d');
-
-    const genetic = new Genetic(context);
-
+    
     const solutionFitness = util.randomColor();
-
-    genetic.run(solutionFitness, buttonPopulation, lblPopulation, lblBestChromosome);
+    
+    const genetic = new Genetic(context, solutionFitness);
+    
+    genetic.nextGeneration(buttonPopulation, lblGeneration, lblBestChromosome);
+    genetic.pointIndividual(canvas, lblIndividualFitness);
 
     lblFitness.innerHTML = `Fitness: <strong>#${solutionFitness}</strong>`;
     fitnessContext.fillStyle = `#${solutionFitness}`;
